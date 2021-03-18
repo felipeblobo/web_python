9c06a1e2943ce84db9ffd2c9d9b9e4e9b91079f8
Felipe Lobo <felipeblobo@gmail.com>
2021-03-18 19:16:08 -0300

    tela de login, importando banco de dados

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,29 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+
 
 @app.route("/")
 @app.route("/index")
-def index(): 
+def index():
     user = {"username": "Felipe"}
     posts = [
         {
             "author": {"username": "Felipe"},
-            "body": "Olá moça"       
-        
+            "body": "Olá moça"
+
         },
 
-         {
+        {
             "author": {"username": "Pedro"},
-            "body": "Olá pessoal!"       
-        
+            "body": "Olá pessoal!"
+
         }
     ]
     return render_template("index.html", user=user, posts=posts)
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print(request.values.get("user"))
+    return render_template("login.html", title="login")
